4baa5fed125c1964d1a290b36571d8f06113ab6a
Olav Jakobson Digranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-02-18 19:37:14 +0000

    Update player.cpp

--- 4_tile_engine/player.cpp
+++ 4_tile_engine/player.cpp
@@ -1,5 +1,7 @@
 //player.cpp
 #include "player.h"
+#include <iostream>
+
 using namespace sf;
 using namespace std;
 
@@ -19,12 +21,15 @@ void Player::Update(double dt) {
 	}
 	move(direction);
 	Entity::Update(dt);
+	//cout << "____" << endl;
+	//cout << direction.x << endl;
+	//cout << direction.y << endl;
 }
 
 Player::Player()
     : _speed(200.0f), Entity(make_unique<CircleShape>(25.f)) {
     _shape->setFillColor(Color::Magenta);
-    _shape->setOrigin(Vector2f(25.f, 25.f));
+    _shape->setOrigin(Vector2f(50.f, 50.f));
 }
 
 void Player::Render(sf::RenderWindow& window) const {
