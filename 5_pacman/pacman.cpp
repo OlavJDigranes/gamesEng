062864ae57688f33d64962283e9cd1721936a6e7
OlavJDigranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-02-22 11:48:58 +0000

    Update pacman.cpp

--- 5_pacman/pacman.cpp
+++ 5_pacman/pacman.cpp
@@ -1,3 +1,13 @@
+void MenuScene::update(double dt) {
+  Scene::update(dt);
+  text.setString("Almost Pacman");
+}
+
+void MenuScene::render() {
+  Renderer::queue(&text);
+  Scene::render();
+}
+
 void MenuScene::Load() {
 	font.loadFromFile("res/fonts/RobotoMono-Bold.ttf");
 	text.setFont(font);
