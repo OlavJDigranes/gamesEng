77fde4c5f94bcd5c6d6645831e59bb792d496dd7
Olav Jakobson Digranes <58626496+OlavJDigranes@users.noreply.github.com>
2022-03-22 14:12:02 +0000

    Attempted LevelSystem fix
    Attempted level system fix of level loading with setting tile size smaller

--- lib_tile_level_loader/LevelSystem.h
+++ lib_tile_level_loader/LevelSystem.h
@@ -11,7 +11,7 @@
 
 class LevelSystem {
 public:
-    static void loadLevelFile(const std::string&, float tileSize = 100.0f);
+    static void loadLevelFile(const std::string&, float tileSize = 50.0f);
     static void unload();
     static void render(sf::RenderWindow& window);
 
